20ddcc2e672189466f7cba1c54def87df512bf10
Mark Johnson <luther07@gmail.com>
2012-09-05 20:23:37 -0500

    These two ruby files are some code that I extended and improved.
    Here are the extensions I made:
    Add a require command to the tests so they can reference tree_node.
    Add test coverage for methods: name and name=.
    Add an instance method that returns true if the argument is its child.
    Add an instance method that returns path to the calling node and write a test.
    Write a method to help depth-first search. The method takes a block
    argument and yields the node and it's descendants in order.

---
+++ code/tree_node.rb
@@ -0,0 +1,70 @@
+class TreeNode
+  attr :children
+  attr_accessor :parent
+  attr_accessor :name
+
+  def initialize(name = nil)
+    @name = name
+    @children = []
+  end
+
+  def add_child(child)
+    if !child.parent.nil?
+      child.parent.remove_child_helper(child)
+    end
+
+    @children << child
+    child.parent = self
+  end
+
+  def remove_child_helper(child)
+    child_array = Array.[](child)
+    @children = @children - child_array
+  end
+
+  def children_count
+    @children.size
+  end
+
+  def child?(node)
+    memberhood = false
+
+    @children.each do |item|
+      if item == node
+        memberhood = true
+      end
+    end
+
+    return memberhood
+  end
+
+  def node_path
+    if parent == nil
+      name.to_s
+    else
+      "#{ parent.node_path } > #{ name }"
+    end
+  end
+
+  # Our data structure is like an upside-down stack.
+  # Children are added and removed at the front.
+  def depth_first_each
+      shift_array = []
+      node = self
+
+    if block_given?
+      shift_array.unshift(node)
+
+      while shift_array.size > 0
+        yield shift_array.shift
+        shift_array = node.children + shift_array
+        node = shift_array[0]
+      end
+
+    return self
+
+    end
+
+  end
+
+end
---
+++ code/tree_node_test.rb
@@ -0,0 +1,128 @@
+require_relative 'tree_node'
+require 'test/unit'
+
+class TreeNodeTest < Test::Unit::TestCase
+
+  def setup
+    @tree_node = TreeNode.new
+    @named_tree_node = TreeNode.new('Homer Jay Simpson')
+    @second_node = TreeNode.new('Bart Simpson')
+    @third_node = TreeNode.new('Bart Simpson II')
+    @A = TreeNode.new('A')
+    @B = TreeNode.new('B')
+    @C = TreeNode.new('C')
+    @D = TreeNode.new('D')
+    @E = TreeNode.new('E')
+    @F = TreeNode.new('F')
+    @G = TreeNode.new('G')
+  end
+
+  def test_has_no_children_when_created
+    assert_equal(0, @tree_node.children_count,
+                 'should have an empty children array when created')
+  end
+
+  def test_has_no_parent_when_created
+    assert_nil(@tree_node.parent,
+               'node has no parent by default')
+  end
+
+  def test_has_no_name_when_created
+    assert_nil(@tree_node.name,
+               'node has no name by default')
+  end
+
+  def test_initialize_node_with_name
+    assert_equal('Homer Jay Simpson', @named_tree_node.name,
+                 'should accept a name during construction')
+  end
+
+  def test_modify_node_name
+    @named_tree_node.name = 'Marjorie "Marge" Simpson'
+
+    assert_equal('Marjorie "Marge" Simpson', @named_tree_node.name,
+                 'should allow you to modify node name')
+  end
+
+  def test_node_path
+    @named_tree_node.add_child(@second_node)
+    @second_node.add_child(@third_node)
+
+    assert_equal('Homer Jay Simpson > Bart Simpson > Bart Simpson II',
+                 @third_node.node_path,
+                 'should return string description of path to node')
+  end
+
+  def test_depth_first_each_block
+    @A.add_child(@B)
+    @A.add_child(@C)
+    @B.add_child(@D)
+    @B.add_child(@E)
+    @E.add_child(@F)
+    @E.add_child(@G)
+
+    assert_equal(@A, @A.depth_first_each { |item| 1 + 1 },
+                 'should return the receiver if a block is passed')
+  end
+
+  def test_depth_first_each_no_block
+    @A.add_child(@B)
+    @A.add_child(@C)
+    @B.add_child(@D)
+    @B.add_child(@E)
+    @E.add_child(@F)
+    @E.add_child(@G)
+
+    assert_equal(nil, @A.depth_first_each,
+                 'should return nil if a no block is passed')
+  end
+
+end
+
+class TreeNodeChildAssignmentTest < Test::Unit::TestCase
+
+  def setup
+    @tree_node = TreeNode.new
+    @second_node = TreeNode.new
+    @child_node = TreeNode.new
+  end
+
+  def test_children_count
+    @tree_node.add_child(@child_node)
+
+    assert_equal(1, @tree_node.children_count,
+                 'should have a single child when one is added')
+  end
+
+  def test_parent_assignment
+    @tree_node.add_child(@child_node)
+
+    assert_equal(@tree_node, @child_node.parent)
+  end
+
+
+  def test_node_for_child
+    @tree_node.add_child(@child_node)
+
+    assert_equal(true, @tree_node.child?(@child_node),
+                 'should indicate if node has specified child')
+  end
+
+  def test_helper_remove_child
+    @tree_node.add_child(@child_node)
+    @tree_node.remove_child_helper(@child_node)
+
+    assert_equal(false, @tree_node.child?(@child_node),
+                 'should remove the specified child node')
+  end
+
+  def test_child_reassignment
+    @tree_node.add_child(@child_node)
+    @second_node.add_child(@child_node)
+
+    assert_equal(false, @tree_node.child?(@child_node),
+                 "child reassignment should remove child from previous /
+                  parents' children")
+  end
+
+end
